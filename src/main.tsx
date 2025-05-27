4686cc6c6d268886df28c0b228a163a1992b4baa
krishan kumar safi <krishankumarsafi7@gmail.com>
2025-05-27 19:32:29 +0530

    add-tailwind-config

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
 import "./index.css";
-import App from "./App.tsx";
 
+import { App } from "./App.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { store } from "./redux/store.ts";
 import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
